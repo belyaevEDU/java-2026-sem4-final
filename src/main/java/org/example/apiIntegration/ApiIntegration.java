ed92a6d4a55fb4ae65c331051439d433f6383649
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-03-06 17:17:30 +0300

    feat: started integrating api interaction

--- src/main/java/org/example/apiIntegration/ApiIntegration.java
+++ src/main/java/org/example/apiIntegration/ApiIntegration.java
@@ -1,4 +1,14 @@
 package org.example.apiIntegration;
 
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
 public class ApiIntegration {
+    private final ApiRecord[] apis;
+
+    public ApiIntegration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        apis = objectMapper.readValue(new File("src/main/resources/apis.json"), ApiRecord[].class);
+    }
 }
--- src/main/java/org/example/apiIntegration/ApiRecord.java
+++ src/main/java/org/example/apiIntegration/ApiRecord.java
@@ -1,4 +1,126 @@
 package org.example.apiIntegration;
 
-public record ApiRecord() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Function;
+
+public final class ApiRecord {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("key_needed")
+    private boolean keyNeeded;
+    @JsonProperty("API_KEY")
+    private String apiKey;
+    @JsonProperty("base_request_url")
+    private String baseRequestURL;
+    @JsonProperty("additional_path_needed")
+    private boolean additionalPathNeeded;
+    @JsonProperty("additional_paths")
+    private String[] additionalPaths;
+
+    private Function<String[], String> handler; // set later
+
+    public ApiRecord() {} // for jackson's objectMapper to work (same thing for the setters)
+
+    public int id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public boolean keyNeeded() {
+        return keyNeeded;
+    }
+
+    protected String apiKey() {
+        return apiKey;
+    }
+
+    public String baseRequestURL() {
+        return baseRequestURL;
+    }
+
+    public boolean additionalPathNeeded() {
+        return additionalPathNeeded;
+    }
+
+    public String[] additionalPaths() {
+        return additionalPaths;
+    }
+
+    public Function<String[], String> handler() {
+        return handler;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setKeyNeeded(boolean keyNeeded) {
+        this.keyNeeded = keyNeeded;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setBaseRequestURL(String baseRequestURL) {
+        this.baseRequestURL = baseRequestURL;
+    }
+
+    public void setAdditionalPathNeeded(boolean additionalPathNeeded) {
+        this.additionalPathNeeded = additionalPathNeeded;
+    }
+
+    public void setAdditionalPaths(String[] additionalPaths) {
+        this.additionalPaths = additionalPaths;
+    }
+
+    public void setHandler(Function<String[], String> handler) {
+        this.handler = handler;
+    }
+
+    // 3 functions below gen'd by IDEA
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ApiRecord) obj;
+        return this.id == that.id &&
+                Objects.equals(this.name, that.name) &&
+                this.keyNeeded == that.keyNeeded &&
+                Objects.equals(this.apiKey, that.apiKey) &&
+                Objects.equals(this.baseRequestURL, that.baseRequestURL) &&
+                this.additionalPathNeeded == that.additionalPathNeeded &&
+                Objects.equals(this.additionalPaths, that.additionalPaths) &&
+                Objects.equals(this.handler, that.handler);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, keyNeeded, apiKey, baseRequestURL, additionalPathNeeded, Arrays.hashCode(additionalPaths), handler);
+    }
+
+    @Override
+    public String toString() {
+        return "ApiRecord[" +
+                "id=" + id + ", " +
+                "name=" + name + ", " +
+                "keyNeeded=" + keyNeeded + ", " +
+                "apiKey=" + apiKey + ", " +
+                "baseRequestURL=" + baseRequestURL + ", " +
+                "additionalPathNeeded=" + additionalPathNeeded + ", " +
+                "additionalPaths=" + Arrays.toString(additionalPaths) + ", " +
+                "handler=" + handler + ']';
+    }
 }
