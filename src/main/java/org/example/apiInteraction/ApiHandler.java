3423828ea23a83dd4206744511cdff548b61aaf0
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-03-06 21:31:41 +0300

    feat: added an apihandler annotation, started structuring api handlers implementation

--- src/main/java/org/example/apiInteraction/ApiHandler.java
+++ src/main/java/org/example/apiInteraction/ApiHandler.java
@@ -1,4 +1,12 @@
 package org.example.apiInteraction;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ApiHandler {
+    int apiId();
 }
--- src/main/java/org/example/apiInteraction/ApiInteraction.java
+++ src/main/java/org/example/apiInteraction/ApiInteraction.java
@@ -1,14 +1,5 @@
 package org.example.apiInteraction;
 
-import tools.jackson.databind.ObjectMapper;
-
-import java.io.File;
-
 public class ApiInteraction {
-    private final ApiRecord[] apis;
 
-    public ApiInteraction() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        apis = objectMapper.readValue(new File("src/main/resources/apis.json"), ApiRecord[].class);
-    }
 }
