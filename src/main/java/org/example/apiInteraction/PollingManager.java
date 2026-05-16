959f1e6a39975bbeed1b86d4446105315144a5f6
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-05-16 14:22:20 +0300

    chore(pollingmanager): remoevd unnecessary import

--- src/main/java/org/example/apiInteraction/PollingManager.java
+++ src/main/java/org/example/apiInteraction/PollingManager.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.*;
